5344efd1aa2089b97f8c0581c1c668075ed81c99
Cams03 <camille.francia@hotmail.fr>
2017-01-11 15:34:52 +0100

    style flèches ok mais bug fleche prev

--- app.js
+++ app.js
@@ -1,23 +1,30 @@
-$(function() {
+$(function () {
 
-    $(".next").click(function(){
+    $(".prev").click(function () {
 
-      $(".slideshow ul").animate({marginLeft:-1000}, 800, function() {
+        $(".slideshow ul").animate({
+            marginRight: -1000
+        }, 800, function () {
 
-        $(this).css({marginLeft: 0}).find("li:last").after($(this).find("li:first"));
+            $(this).css({
+                marginRight: 0
+            }).find("li:first").after($(this).find("li:last"));
 
-      })
+        })
 
     })
 
-    $(".prev").click(function(){
+    $(".next").click(function () {
 
-      $(".slideshow ul").animate({marginRight:-1000}, 800, function() {
+        $(".slideshow ul").animate({
+            marginLeft: -1000
+        }, 800, function () {
 
-        $(this).css({marginRight: 0}).find("li:first").after($(this).find("li:last"));
+            $(this).css({
+                marginLeft: 0
+            }).find("li:last").after($(this).find("li:first"));
 
-      })
-
+        })
     })
 
 });
